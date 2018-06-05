5e2afeab78715dc65205cb43d094ec061e1c7645
rmedard <00Derfine@5>
2018-06-05 22:12:09 +0200

    W10

--- src/main/java/com/sic/jobboard/repository/JobsRepository.java
+++ src/main/java/com/sic/jobboard/repository/JobsRepository.java
@@ -1,9 +1,9 @@
 package com.sic.jobboard.repository;
 
 import com.sic.jobboard.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobsRepository extends JpaRepository<Job, Long> {
+public interface JobsRepository extends CrudRepository<Job, Long> {
 }
--- src/main/java/com/sic/jobboard/repository/RolesRepository.java
+++ src/main/java/com/sic/jobboard/repository/RolesRepository.java
@@ -1,9 +1,9 @@
 package com.sic.jobboard.repository;
 
 import com.sic.jobboard.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RolesRepository extends JpaRepository<Role, Long> {
+public interface RolesRepository extends CrudRepository<Role, Long> {
 }
